eca50f746aaa0c71b9a1c160ae94af28a6bfb819
Bharat Verma <bindian0509@gmail.com>
2022-07-04 23:54:45 +0530

    Added names to containers and commands used

---
+++ docker-compose/chapter_commands.sh
@@ -0,0 +1,13 @@
+docker-compose up 
+docker-compose up -d 
+
+
+docker-compose down 
+# to remove volumes as well 
+docker-compose down -v 
+
+# force build 
+docker-compose up --build
+
+# only build no container 
+docker-compose build 
