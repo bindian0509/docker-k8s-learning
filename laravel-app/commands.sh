0f71e62a4c0ca03816756630f4f4c53a988034f9
Bharat Verma <bindian0509@gmail.com>
2022-07-06 00:31:40 +0530

    Ending Section 8: Complex laravel application

---
+++ laravel-app/commands.sh
@@ -0,0 +1,5 @@
+# to start laravel project 
+docker-compose run --rm composer create-project --prefer-dist laravel/laravel .
+
+# to run the laravel app (with depends) 
+docker-compose up --build server                                                                                                                                                              
--- laravel-app/docker-files/php.dockerfile
+++
@@ -1,5 +0,0 @@
-FROM php:8.0.21RC1-zts-alpine3.16
-
-WORKDIR /var/www/html 
-
-RUN docker-php-ext-install pdo pdo_mysql 
