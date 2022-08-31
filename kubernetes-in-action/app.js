f515be4aed69601ea6a29f7162cc62e82ad76597
Bharat Verma <bindian0509@gmail.com>
2022-08-31 14:14:04 +0530

    Tried K8s with imperative approach with minikube

--- kubernetes-in-action/app.js
+++ kubernetes-in-action/app.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello from this NodeJS app!</h1>
-    <p>Try sending a request to /error and see what happens</p>
+    <h1>Update v2.0 - Hello from this NodeJS app!</h1>
+    <p>Try sending a request to /error and see what happens haha !! </p>
   `);
 });
 
